51ae93886cb547eb9db87e939d4cf989b24d0ba3
Aviv Levinsky <pugsiman@gmail.com>
2015-12-13 13:41:01 +0200

    Final fix for the heroku bug with migration references

--- db/migrate/20151211132857_add_creator_ref_to_event.rb
+++ db/migrate/20151211132857_add_creator_ref_to_event.rb
@@ -1,5 +1,6 @@
 class AddCreatorRefToEvent < ActiveRecord::Migration
   def change
-    add_reference :events, :creator, index: true, foreign_key: true, unique: true
+    add_column :events, :creator_id, :integer
+    add_index :events, :creator_id, unique: true
   end
 end
