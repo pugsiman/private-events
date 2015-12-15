3cedece3fa571a8aba7c8ca720e66c66217141d5
Aviv Levinsky <pugsiman@gmail.com>
2015-12-15 03:04:05 +0200

    Add customized navigation bar and special helper methods for navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def current_page(path)
+    'current' if current_page?(path)
+
+    # TODO: make it not buggy:
+    # 'current' if request.url.include?(path)
+  end
+
+  # helper method following DRY principle. The string is the same
+  # in both the hyperlink-text and the data-hover html attribute.
+  def create_nav_link(string, path, method)
+    link_to string, path, data: { hover: string }, method: method
+  end
+
+  # helper method following DRY principle using both mentioned methods.
+  # consider that both nav_link and current_page methods are using the same
+  # path variable.
+  def create_nav_item(string, path, method = nil)
+    content_tag :li, create_nav_link(string, path, method), class: current_page(path)
+  end
 end
