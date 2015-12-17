aff4212df051e281f1d8dbb0b6d56756920e09bd
Aviv Levinsky <pugsiman@gmail.com>
2015-12-17 21:27:45 +0200

    Add stylizing for sign up page and a bit general scss tweaking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: :profile
 
   def new
-    if logged_in?
-      flash[:info] = 'You are already logged in.'
-      redirect_to root_url
-    end
+    redirect_to root_url if logged_in?
     @user = User.new
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,3 +2,8 @@ User.create!(name:  'Aviv',
              email: 'pugsiman@gmail.com',
              password:              '123456',
              password_confirmation: '123456')
+
+User.create!(name:  'Avi',
+             email: 'avi@gmail.com',
+             password:              '123456',
+             password_confirmation: '123456')
